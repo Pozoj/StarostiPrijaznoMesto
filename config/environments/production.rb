7f9496a402e187c8c0c77df4f54b4247fca75de5
Islam Mušić <islam.music@gmail.com>
2013-02-21 12:16:10 +0100

    pošiljanje pošte + pdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,19 @@ Starci::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'beta.starostiprijazno.si',
+      :user_name            => 'islam.music',
+      :password             => 'K0ru7a+4',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
+  config.action_mailer.default_url_options = { :host => 'beta.starostiprijazno.si' }
+
 end
