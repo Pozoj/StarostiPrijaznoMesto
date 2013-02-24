6304e5693e32a3949b067c94b355a505cb89b5bd
Miha Rebernik <miha@rebernik.info>
2013-02-24 12:31:11 -0800

    Fixing mailer.

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -1,15 +1,10 @@
 class AnswerMailer < ActionMailer::Base
-  default from: "info@starostiprijaznomesto.si", :bcc => "Miha Rebernik <rasmajx@gmail.com>, Islam Music <islam.music@gmail.com>"
+  default from: "Starosti Prijazno <info@starostiprijazno.si>", :bcc => "Miha Rebernik <rasmajx@gmail.com>, Islam Music <islam.music@gmail.com>"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.answer_mailer.send_answer_form.subject
-  #
   def send_answer_form(user, post, text)
     @user = user
     @post = post
     @text = text
-    mail to: user.email   , subject: "Prosimo za odgovor"
+    mail to: user.email, subject: "Prosimo za odgovor"
   end
 end
