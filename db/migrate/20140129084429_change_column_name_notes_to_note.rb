c1ab9b2b7158ac5a6264dc51364b48057a935b58
Islam Mušić <islam.music@gmail.com>
2014-01-29 09:48:42 +0100

    preimenovanje

--- db/migrate/20140129084429_change_column_name_notes_to_note.rb
+++ db/migrate/20140129084429_change_column_name_notes_to_note.rb
@@ -1,9 +1,9 @@
 class ChangeColumnNameNotesToNote < ActiveRecord::Migration
   def up
-    rename_column :answer, :notes, :note
+    rename_column :answers, :notes, :note
   end
 
   def down
-    rename_column :answer, :note, :notes
+    rename_column :answers, :note, :notes
   end
 end
