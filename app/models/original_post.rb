549b16c908b4ef4012cc595604030904f586de02
Miha Rebernik <miha@rebernik.info>
2014-04-21 18:24:00 +0200

    Anti-spam honeypot implementation.

--- app/models/original_post.rb
+++ app/models/original_post.rb
@@ -1,11 +1,12 @@
 class OriginalPost < ActiveRecord::Base
+  attr_accessor :notes
+  
   has_one :posts
   has_many :attachments, :as => :holder, :dependent => :destroy
   #attr_accessible :note
   validates_presence_of :first_name, :last_name, :text
+  validate :exclude_spammers
   accepts_nested_attributes_for :attachments
-
-
   
   class << self
     
@@ -15,6 +16,11 @@ class OriginalPost < ActiveRecord::Base
       joins("LEFT JOIN posts ON original_posts.id = posts.original_post_id").where("posts.id IS NULL") #.order("created_at DESC").group_by {|og_post| og_post.created_at.to_date }
     end
   end # class << self
+
+  def exclude_spammers
+    return unless notes.present?
+    errors.add :base, 'This is unacceptable behaviour, yo!'
+  end
 
   def to_s
     text
